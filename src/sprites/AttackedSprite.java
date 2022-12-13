0a450225a560c852f774fc6546c11858ac73f59c
oreo10baht <papigonwin001@gmail.com>
2022-12-13 19:49:38 +0700

    sprites

--- src/sprites/AttackedSprite.java
+++ src/sprites/AttackedSprite.java
@@ -5,7 +5,7 @@ import javafx.scene.image.ImageView;
 
 public class AttackedSprite extends ImageView {
 	public AttackedSprite(String name) {
-		Image img = new Image(ClassLoader.getSystemResource(name + "Attacked.png").toString());
+		Image img = new Image(ClassLoader.getSystemResource(name + "Attacked.gif").toString());
 		setImage(img);
 		setSizeByName(name);
 	}
--- src/sprites/CorpseSprite.java
+++ src/sprites/CorpseSprite.java
@@ -4,10 +4,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class CorpseSprite extends ImageView {
-	public class IdleSprite extends ImageView{
-		public IdleSprite(String name) {
-			Image img = new Image(ClassLoader.getSystemResource(name + "Idle.gif").toString());
+		public CorpseSprite(String name) {
+			Image img = new Image(ClassLoader.getSystemResource(name + "Corpse.gif").toString());
 			setImage(img);
-			//setSizeByName(name);
+			setSizeByName(name);
 		}
-}
+		public void setSizeByName(String name) {
+			
+		}
+};
